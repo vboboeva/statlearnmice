fdec3bb3cfd3f8520231d659c7f9d4adac6e718d
vboboeva <vezha.boboeva@gmail.com>
2025-05-27 17:51:19 +0100

    fixed z-scoring

--- mice_analysis.py
+++ mice_analysis.py
@@ -5,46 +5,52 @@ from sklearn.decomposition import PCA
 import pickle
 from os.path import join
 from collections import defaultdict
-from sklearn.preprocessing import StandardScaler
+import scipy
+from scipy.stats import zscore
 
-#centering and z-scoring functions
-def center(data):
-	ss = StandardScaler(with_mean=True, with_std=False)
-	return ss.fit_transform(data.T).T
-
-def z_score(data):
-	data = np.array(data)
-	print(type(data))
-	ss = StandardScaler(with_mean=True, with_std=True)
-	return ss.fit_transform(data.T).T
-
-def make_pseudopop(seq_types, unique_mouse_ids, sessions_by_id, data):
+def make_pseudopop(seq_types, mouse_ids, sessions_by_id, data):
 	# make a pseudopopulation of neural data by averaging across trials for each mouse and session
-	# and stacking them together
+	# and stacking them together: diff sessions diff neurons
 	# data is a dictionary with keys as 'mouse_id_session' and values as a dictionary with keys as sequence types
 	# and values as neural data (num_trials, num_neurons, num_timepts)
 	# seq_types is a list of sequence types (e.g. ['A-0', 'A-1', 'A-2', 'A-3'])
-	# unique_mouse_ids is a list of unique mouse ids
+	# mouse_ids is a list of mouse ids to run analysis on
+	which_zscore = 'across_time'  # options: 'across_trials_and_time', 'across_trials', 'across_time'
 	data_pseudopop_temp = []
 	for seq_type in seq_types:
-		for mouse_id in unique_mouse_ids:
-			# print(mouse_id)
+		print(seq_type)
+		for mouse_id in mouse_ids:
+			print(mouse_id)
 			for session in sessions_by_id[mouse_id]:
-				# print(session)
+				print(session)
 				key = f'{mouse_id}' + '_' + f'{session}'
-				data_single_sess = data[key][seq_type] 	# print(num_trials, num_neurons, num_timepts)
-				data_single_sess_avg_across_trials = np.mean(data_single_sess, axis=0)
-				# if (np.shape(data_single_sess_avg_across_trials)[0] > 100):
-				# 	print(key, seq_type)
-				# 	print(np.shape(data_single_sess_avg_across_trials)[0])
+				data_single_sess = data[key][seq_type] 	
+				num_trials, num_neurons, num_timepts = data_single_sess.shape
+				if which_zscore == 'across_trials_and_time':
+					data_single_sess = data_single_sess.transpose(0, 2, 1)  # shape (num_trials, num_timepts, num_neurons)
+					data_single_sess = data_single_sess.reshape(-1, num_neurons)
+					data_single_sess = zscore(data_single_sess, axis=0)  # z-score across neurons
+					data_single_sess = data_single_sess.reshape(num_trials, num_timepts, num_neurons)
+					data_single_sess = data_single_sess.transpose(0, 2, 1)  # reshape (num_trials, num_neurons, num_timepts)
+				elif which_zscore == 'across_trials':
+					data_single_sess = zscore(data_single_sess, axis=0)  # z-score across neurons
+				elif which_zscore == 'across_time':
+					data_single_sess = zscore(data_single_sess, axis=1)
+
+				data_single_sess[np.isnan(data_single_sess)] = 0.000
+				data_single_sess_avg_across_trials = np.nanmean(data_single_sess, axis=0)
 				data_pseudopop_temp.append(data_single_sess_avg_across_trials)
 
-
 	data_pseudopop_stacked = np.vstack(data_pseudopop_temp)
-	data_pseudopop_stacked = z_score(data_pseudopop_stacked)
+	print(np.shape(data_pseudopop_stacked))
+	print(data_pseudopop_stacked)
+	exit()
+
 	assert data_pseudopop_stacked.shape[0] % len(seq_types) == 0
+
 	data_pseudopop = data_pseudopop_stacked.reshape(len(seq_types), data_pseudopop_stacked.shape[0] // 4, data_pseudopop_stacked.shape[1])
 	data_pseudopop = data_pseudopop.transpose(0,2,1) # putting N at the last dimension
+
 
 	return data_pseudopop	
 
@@ -82,6 +88,8 @@ if __name__ == "__main__":
 
 	mouse_list = [[x] for x in unique_mouse_ids] + [unique_mouse_ids]
 	fignames = np.append(unique_mouse_ids, 'all_mice')
+	print(mouse_list)
+	# exit()
 	print(fignames)
 
 	for mouse_id, figname in zip(mouse_list, fignames):
@@ -89,24 +97,27 @@ if __name__ == "__main__":
 		data_pseudopop = make_pseudopop(seq_types, mouse_id, sessions_by_id, data)
 
 		_p, _t, _N = data_pseudopop.shape
-		print(_p,_t,_N)
+		print('number of seqtypes=',_p)
+		print('number of timepts=',_t)
+		print('number of neurons',_N)
 
 		pca = PCA(n_components = n_components)
 		# find principal components for a particular position in sequence 
 		_ = pca.fit_transform(data_pseudopop.reshape(-1, _N))
 		# project all sequences along them
 		data_embedding = pca.transform(data_pseudopop.reshape(-1, _N))
+		print('shape data_embedding', np.shape(data_embedding))
+		print('shape data_pseudopop', np.shape(data_pseudopop))
 
-		proj_variance = np.sum(np.var(data_embedding, axis=0))
+		proj_variance = np.cumsum(np.var(data_embedding, axis=0))
 		total_variance = np.sum(np.var(data_pseudopop.reshape(-1, _N), axis=0))
-		frac_variance = proj_variance / total_variance
+		frac_variance = proj_variance / total_variance*np.ones(len(proj_variance))
 
-		print("total_variance =", total_variance)
-		print("proj_variance =", proj_variance)
+		print("proj_variance =", np.shape(proj_variance))
+		print("total_variance =", np.shape(total_variance))
 		print("frac_variance =", frac_variance)
 
 		data_embedding = data_embedding.reshape(_p, _t, n_components)
-		print(np.shape(data_embedding))
 
 		fig, ax = plt.subplots(2,5, figsize=(20, 10))
 		ax = ax.flatten()
@@ -120,11 +131,11 @@ if __name__ == "__main__":
 				ax[comp].axvspan(25+25, 40+25, color='red', lw=0, alpha=0.05)
 				ax[comp].axvspan(50+25, 65+25, color='red', lw=0, alpha=0.05)
 				ax[comp].axvspan(75+25, 90+25, color='red', lw=0, alpha=0.05)
-				ax[comp].suptitle(f'PC {comp+1} ({frac_variance:.2f} variance)', fontsize=16)
+				ax[comp].set_title(f'PC {comp+1} ({frac_variance[comp]:.2f} variance)', fontsize=16)
 			
 		ax[0].legend()
 		fig.suptitle(f'{_N} neurons across sessions')
-		plt.savefig(f'pca_components_ABCD_vs_ABBA_{figname}.svg', bbox_inches='tight')
+		fig.savefig(f'pca_components_ABCD_vs_ABBA_{figname}.svg', bbox_inches='tight')
 
 			
 
